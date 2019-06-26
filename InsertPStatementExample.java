55b81d52f3c3a95f183b1db051c2ca03a537f43b
kofijr <calebasanteawuah@gmail.com>
2019-06-26 11:43:26 +0000

    sql statement example work

---
+++ InsertPStatementExample.java
@@ -0,0 +1,60 @@
+package com.javaguides.jdbc.preparestatement.examples;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+public class InsertPStatementExample {
+    private static final String INSERT_USERS_SQL = "INSERT INTO users" +
+        "  (id, name, email, country, password) VALUES " +
+        " (?, ?, ?, ?, ?);";
+
+    public static void main(String[] argv) throws SQLException {
+        InsertPStatementExample createTableExample = new InsertPStatementExample();
+        createTableExample.insertRecord();
+    }
+
+    public void insertRecord() throws SQLException {
+        System.out.println(INSERT_USERS_SQL);
+
+        try (Connection connection = DriverManager
+            .getConnection("jdbc:mysql://localhost:3306/mysql_database?useSSL=false", "kisssmyasss", "qwerr11234");
+
+
+            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL)) {
+            preparedStatement.setInt(1, 1);
+            preparedStatement.setString(2, "kofi");
+            preparedStatement.setString(3, "kofi@gmail.com");
+            preparedStatement.setString(4, "Ghana");
+            preparedStatement.setString(5, "123456789");
+
+            System.out.println(preparedStatement);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+
+
+            printSQLException(e);
+        }
+
+
+    }
+
+    public static void printSQLException(SQLException ex) {
+        for (Throwable e: ex) {
+            if (e instanceof SQLException) {
+                e.printStackTrace(System.err);
+                System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+                System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+                System.err.println("Message: " + e.getMessage());
+                Throwable t = ex.getCause();
+                while (t != null) {
+                    System.out.println("Cause: " + t);
+                    t = t.getCause();
+                }
+            }
+        }
+    }
+}
